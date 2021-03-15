64395eeae25d4a44d081731c8aac62a32fbca379
johnhongannguyen <johhaneshongan@gmail.com>
2021-03-15 00:37:51 -0700

    adding Button component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,95 @@
 
 import './App.css';
 import Header from './components/Header'
+import Item from './components/Item';
+
+import Accessories from './assets/Desktop-Accessories.jpg';
+import ModelS from './assets/Desktop-ModelS.jpeg';
+import Model3 from './assets/Desktop-Model3.jpeg';
+import ModelY from './assets/Desktop-ModelY.jpeg';
+import ModelX from './assets/Desktop-ModelX.jpeg';
+import SolarPanels from './assets/Desktop-SolarPanels.jpeg';
+import SolarRoof from './assets/Desktop-SolarRoof.jpeg';
 
 function App() {
   return (
     <div className="App">
      <Header />
-      {/* item  => snapping */}
+      <div className='app__itemsContainer'>
+        <Item 
+          title='Lower Cost Solar Panels in America'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={SolarPanels}
+          leftBtnTxt='ORDER NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+          first
+        />
+          <Item 
+          title='Model S'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={ModelS}
+          leftBtnTxt='ORDER NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+          first
+        />
+          <Item 
+          title='Model 3'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={Model3}
+          leftBtnTxt='ORDER NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+          first
+        />
+              <Item 
+          title='Model X'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={ModelX}
+          leftBtnTxt='ORDER NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+          first
+        />
+          <Item 
+          title='Model Y'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={ModelY}
+          leftBtnTxt='ORDER NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+          first
+        />
+            <Item 
+          title='Solar for New Roofs'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={SolarRoof}
+          leftBtnTxt='ORDER NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+          first
+        />
+        
+      </div>
       
     </div>
   );
--- src/components/item.js
+++ src/components/item.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import Button from './Button';
+import {findAllByTitle} from '@testing-library/react';
 
-const Item = () =>{
+const Item = ({title,desc, backgroundImg, leftBtnTxt, leftBtnLink, rightBtnTxt, rightBtnLink,twoButtons, first}) =>{
     return(
         <div className='item' style={{
 
         }}>
             <div className='item__container'>
                 <div className='item__text'>
-                    <p>Title</p>
+               
+                    <p>{title}</p>
                     <div className='item__textDesc'>
-                        <p>Description</p>
+                        <p>{desc}</p>
 
                     </div>
                 </div>
                 <div className='item__lowerThird'>
                     <div className='item__button'>
-                        
+                        <Button imp='primary' text={leftBtnLink} link={leftBtnLink} />
+                            
                     </div>
                 </div>
             </div>
