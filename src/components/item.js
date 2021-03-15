19fe34383e003772dbbdbe64e1285b4b53674ebe
johnhongannguyen <johhaneshongan@gmail.com>
2021-03-14 23:54:55 -0700

    set skeleton for items

---
+++ src/components/item.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Item = () =>{
+    return(
+        <div className='item' style={{
+
+        }}>
+            <div className='item__container'>
+                <div className='item__text'>
+                    <p>Title</p>
+                    <div className='item__textDesc'>
+                        <p>Description</p>
+
+                    </div>
+                </div>
+                <div className='item__lowerThird'>
+                    <div className='item__button'>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Item;
